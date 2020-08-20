3510f8a66f8035bc52a74cf0a2266a8356e6fe9a
Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>
2020-08-20 10:24:33 +0200

    ci: Rewrite using extends
    Signed-off-by: Nobuhiro Iwamatsu <nobuhiro1.iwamatsu@toshiba.co.jp>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- scripts/deploy-cip-core.sh
+++ scripts/deploy-cip-core.sh
@@ -16,9 +16,13 @@ fi
 
 RELEASE=$1
 TARGET=$2
-DTB=$3
+EXTENSION=$3
+DTB=$4
 
 BASE_PATH=build/tmp/deploy/images/$TARGET/cip-core-image-cip-core-$RELEASE-$TARGET
+if [ "${EXTENSION}" != "base" ] ; then
+	BASE_PATH=build/tmp/deploy/images/$TARGET/cip-core-image-cip-core-$RELEASE-$TARGET-$EXTENSION
+fi
 
 echo "Compressing cip-core-image-cip-core-$RELEASE-$TARGET.wic.img..."
 xz -9 -k $BASE_PATH.wic.img
@@ -38,6 +42,6 @@ fi
 aws s3 cp --no-progress $KERNEL_IMAGE s3://download.cip-project.org/cip-core/$TARGET/
 aws s3 cp --no-progress $BASE_PATH-initrd.img s3://download.cip-project.org/cip-core/$TARGET/
 
-if [ -n "$DTB" ]; then
+if [ "$DTB" != "none" ]; then
 	aws s3 cp --no-progress build/tmp/work/cip-core-*/linux-cip*/*/linux-cip-*/debian/linux-image-cip*/usr/lib/linux-image-*/$DTB s3://download.cip-project.org/cip-core/$TARGET/
 fi
