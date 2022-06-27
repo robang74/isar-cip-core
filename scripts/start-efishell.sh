6a5c9f7e493134670c6e9ea9480236afb3a525e8
sarath P T <Sarath_PT@mentor.com>
2022-06-27 11:31:05 +0200

    scripts/start-efishell.sh: Add -nographic option
    Add -nographic option to support running the
    script over ssh.
    
    Signed-off-by: sarath P T <Sarath_PT@mentor.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- scripts/start-efishell.sh
+++ scripts/start-efishell.sh
@@ -2,7 +2,7 @@
 ovmf_code=${OVMF_CODE:-/usr/share/OVMF/OVMF_CODE.secboot.fd}
 ovmf_vars=${OVMF_VARS:-./OVMF_VARS.fd}
 DISK=$1
-qemu-system-x86_64 -enable-kvm -M q35 \
+qemu-system-x86_64 -enable-kvm -M q35 -nographic \
                    -cpu host,hv_relaxed,hv_vapic,hv-spinlocks=0xfff -smp 2 -m 2G -no-hpet \
                    -global ICH9-LPC.disable_s3=1 \
                    -global isa-fdc.driveA= \
